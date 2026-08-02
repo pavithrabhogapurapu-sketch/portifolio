8cb8b5a1d757f7d1ef305b1097c7601d7cb79894
pavithrabhogapurapu-sketch <pavithrabhogapurapu@gmail.com>
2026-08-02 12:23:18 +0530

    js code

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,9 @@
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
 // =========================
 // PROJECT POPUP
 // =========================
